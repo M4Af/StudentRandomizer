17dc7bb62e77dcc3a870e9880b6962f2ec7a9af8
M4Af <147251026+M4Af@users.noreply.github.com>
2026-03-26 11:06:17 +0100

    Add files via upload

---
+++ Models/DrawingHistory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.Models
+{
+    public class DrawingHistory
+    {
+        public string ClassName { get; set; }
+        public Student DrawnStudent { get; set; }
+        public DateTime DrawnTime { get; set; }
+        public bool IsLuckyNumber { get; set; }
+        public bool StudentWasPresent { get; set; }
+
+        public DrawingHistory(string className, Student student, bool isLucky = false, bool wasPresent = true)
+        {
+            ClassName = className;
+            DrawnStudent = student;
+            DrawnTime = DateTime.Now;
+            IsLuckyNumber = isLucky;
+            StudentWasPresent = wasPresent;
+        }
+
+        public override string ToString()
+        {
+            return $"{ClassName}|{DrawnTime:yyyy-MM-dd HH:mm:ss}|{DrawnStudent.Name}|{IsLuckyNumber}|{StudentWasPresent}";
+        }
+    }
+}
---
+++ Models/StudentClass.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp3.Models
+{
+    public class StudentClass
+    {
+        public string ClassName { get; set; }
+        public List<Student> Students { get; set; }
+        public int LuckyNumber { get; set; }
+
+        public StudentClass(string className)
+        {
+            ClassName = className;
+            Students = new List<Student>();
+            LuckyNumber = GenerateRandomLuckyNumber();
+        }
+
+        private int GenerateRandomLuckyNumber()
+        {
+            return new Random().Next(1, 36);
+        }
+
+        public override string ToString()
+        {
+            return ClassName;
+        }
+
+        public string ToFileFormat()
+        {
+            return $"CLASS:{ClassName}|LUCKY:{LuckyNumber}";
+        }
+
+        public static StudentClass ParseFromFile(string line)
+        {
+            var parts = line.Split('|');
+            if (parts.Length >= 1)
+            {
+                var className = parts[0].Replace("CLASS:", "");
+                var studentClass = new StudentClass(className);
+
+                if (parts.Length > 1)
+                {
+                    var luckyPart = parts[1].Replace("LUCKY:", "");
+                    studentClass.LuckyNumber = int.Parse(luckyPart);
+                }
+
+                return studentClass;
+            }
+            return null;
+        }
+    }
+}
